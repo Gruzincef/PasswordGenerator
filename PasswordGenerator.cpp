1603e33c1c1fb9057fec9cc4609470e1da7f8a7e
Gruzincef <gruzincef@mail.ru>
2023-03-04 16:08:45 +0700

    Расширен функционал, можно указывать количество каждого типа сиволов  которые должны присутствовать в пароле

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -5,35 +5,80 @@ using namespace std;
 
 string generatorPassword(int count, string base) {
     srand(rand() + time(0));
-    int max = strlen(base.c_str())+1;
+    int max = strlen(base.c_str());
     string password = "";
     for (int i = 0; i < count; i++)
        password = password + base[rand() % (max)];
     return password;
  }
 
+string mixString(string  text)
+{
+    srand(rand() + time(0));
+    int max = strlen(text.c_str());
+    int firstPosition;
+    int nextPosition;
+    char ch;
+    for (int i = 0; i < max; i++)
+    {
+        firstPosition =rand() % (max);
+        nextPosition =rand() % (max);
+        ch = text[firstPosition];
+        text[firstPosition] = text[nextPosition];
+        text[nextPosition] = ch;
+    }
+    return  text;
+}
+
 string generatorPassword(int count, const char* base) {
     srand(rand()+time(0));
-    int max = strlen(base) + 1;
+    int max = strlen(base);
     string password = "";
     for (int i = 0; i < count; i++)
         password = password + base[rand() % (max)];
     return password;
 }
 
+
+
+string generatorPassword(int countNumber, int countSpecial, int countUppercase, int countLowercase) {
+    string Number = "1234567890";
+    string Special = "/!@#$%^&*()_+{}:<>?[];',.";
+    string Uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    string Lowercase = "qwertyuiopasdfghjklzxcvbnm";
+    string password="";
+    password = password + generatorPassword(countNumber, Number);
+    password = password + generatorPassword(countSpecial, Special);
+    password = password + generatorPassword(countUppercase, Uppercase);
+    password = password + generatorPassword(countLowercase, Lowercase);
+    
+    
+    return password;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    const char* base= "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_+{}:<>?[];',./";
-    int countpass;
-    int countchar;
+
     cout << "Введите количество требуемых паролей:";
+    int countpass;
     cin >> countpass;
-    cout << "Введите количество символов в пароле:";
-    cin >> countchar;
+    cout << "Введите количество чисел в пароле:";
+    int countNumber;
+    cin >> countNumber;
+    cout << "Введите количество спец символов в пароле:";
+    int countSpecial;
+    cin >> countSpecial;
+    cout << "Введите количество прописных букв в пароле:";
+    int countUppercase;
+    cin >> countUppercase;
+    cout << "Введите количество строчных букв в пароле:";
+    int countLowercase;
+    cin >> countLowercase;
+    string s = "";
     for (int i = 0; i < countpass; i++)
     {
-        cout << generatorPassword(countchar, base) << endl;
+          cout << mixString(generatorPassword(countNumber, countSpecial, countUppercase, countLowercase)) << endl;
     }
     
 }
